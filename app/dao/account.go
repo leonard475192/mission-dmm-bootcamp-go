74e818158291aead6c9bb35182cf1891543d8d73
leonard475192 <leonard.t1028@gmail.com>
2021-09-05 17:06:47 +0900

    add:account create

--- app/dao/account.go
+++ app/dao/account.go
@@ -23,6 +23,26 @@ func NewAccount(db *sqlx.DB) repository.Account {
 	return &account{db: db}
 }
 
+// Create User :
+func (r *account) Create(ctx context.Context, user object.Account) (*object.Account, error) {
+	query, err := r.db.NamedExecContext(ctx, `INSERT INTO account (username, password_hash) VALUES (:username,:password_hash)`, user)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+	id, err := query.LastInsertId()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	entity := new(object.Account)
+	err = r.db.QueryRowxContext(ctx, "select * from account where id = ?", id).StructScan(entity)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return entity, nil
+}
+
 // FindByUsername : ユーザ名からユーザを取得
 func (r *account) FindByUsername(ctx context.Context, username string) (*object.Account, error) {
 	entity := new(object.Account)
